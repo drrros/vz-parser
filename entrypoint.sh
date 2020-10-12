4031fbb693da2a8433105f770df51b04aa4277d3
root <admin@drros.ru>
2020-10-12 14:24:30 +0400

    Initial gh commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,4 @@ python manage.py collectstatic --noinput --clear
 rm /code/celerybeat.pid
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-# start celery worker
-#celery --app=vz_parser_frontend worker --loglevel=DEBUG &
-# start celery beat
-#celery --app=vz_parser_frontend beat --loglevel=DEBUG &
 exec "$@"
